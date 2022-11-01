3310708ab8f3d3a2c482853e8e15685e06c77296
MHaddi1 <muhammadhaddibro@gmail.com>
2022-11-01 19:42:18 +0500

    Bottom naigation bar added

--- app/src/main/java/com/projectmanager/teamup/MainActivity.java
+++ app/src/main/java/com/projectmanager/teamup/MainActivity.java
@@ -1,14 +1,55 @@
 package com.projectmanager.teamup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.navibottom);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if(id==R.id.home){
+                    loadFragment(new HomePageFragment(),true);
+                }else if(id == R.id.CreateProject){
+                    loadFragment(new CreateProjectFragment(),false);
+                }else if(id == R.id.setting){
+                    loadFragment(new SettingPageFragment(),false);
+                }
+                else {
+                    loadFragment(new ProfilePageFragment(),false);
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+    public void loadFragment(Fragment fragment, boolean flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag==true){
+            ft.add(R.id.container,fragment);
+        }
+        else {
+            ft.replace(R.id.container,fragment);
+        }
+        ft.commit();
     }
 }
---
+++ app/src/main/java/com/projectmanager/teamup/SettingPageFragment.java
@@ -0,0 +1,27 @@
+package com.projectmanager.teamup;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingPageFragment extends Fragment {
+
+
+    public SettingPageFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_page, container, false);
+    }
+}
